d91f23f9e73e2ec03375e43a2a7397f7463c1a5e
guidone <guido.bellomo@gmail.com>
2022-12-15 09:15:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import _ from 'lodash';
+
+import { plug } from 'code-plug';
+
+import { Contents } from '../../src/components/content';
+
+plug(
+  'sidebar',
+  null,
+  {
+    id: 'help-pages',
+    label: 'Help pages',
+    url: '/help',
+    icon: 'file-text',
+    options: [
+      { label: 'Posts', url: '/help', id: 'help-pages' }
+    ]
+  }
+);
+plug('pages', Contents, {
+  url: '/help',
+  title: 'Help pages',
+  id: 'help-pages',
+  namespace: 'help-pages',
+  labels: {
+    createContent: 'Create help page',
+    emptyContent: 'No help pages',
+    saveContent: 'Save help page'
+  }
+});
